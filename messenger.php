fe53f7be3fc97954c01a5480756174900ee74eb8
mar4elkin <googlcraftlol@gmail.com>
2019-09-17 01:16:55 +0300

    header location email sender

--- messenger.php
+++ messenger.php
@@ -5,12 +5,17 @@
   $email = $_POST["email"];
 
   $text = $_POST["text"];
-  
+
+  $to      = 'marktamarov2001@gmail.com';
+  $subject = 'Запрос';
+  $message = 'имя:' . $name . ' , адресс:' . $email . ' , текст:' . $text .'';
+  mail($to, $subject, $message);
+
   $to      = 'marktamarov2001@gmail.com';
   $subject = 'Запрос';
   $message = 'имя:' . $name . ' , адресс:' . $email . ' , текст:' . $text .'';
   mail($to, $subject, $message);
 
 
- //header("Location: https://queryberry.github.io/QueryBerry/");
+ header("Location: https://queryberry.site");
 ?>

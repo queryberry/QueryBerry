ac60284ce40b192978d5acf7f5e7345ba4c3128b
mar4elkin <googlcraftlol@gmail.com>
2019-09-27 16:51:55 +0300

    дабовление header

--- admin/edit_text_page_in.php
+++ admin/edit_text_page_in.php
@@ -22,6 +22,8 @@
                 <form id="groups_and_pagers" class="form-signin"  action="../admin/edit_text_page_maker.php?id=' . $id_page . '" method="post" enctype="multipart/form-data" name="form_auth">
                   <h6 class="h5 mb-3 font-weight-small" style="margin-bottom:10px;">Заголовок:</h6>
                   <input type="text_header" name="text_header" class="form-control" placeholder="' . $row["text_header"] . '"required style="margin-bottom:10px;">
+                  <h6 class="h5 mb-3 font-weight-small" style="margin-bottom:10px;">Превью текст:</h6>
+                  <input type="header" name="header" class="form-control" placeholder="' . $row["header"] . '" required style="margin-bottom:10px;">
                   <textarea type="texta" name="texta" class="suitup-textarea" placeholder="" >' . html_entity_decode($row["text"]) . '</textarea>
                   <input class="btn btn-lg btn-primary btn-block" type="submit" name="btn_submit_register" value="сохранить" id="sub" style="margin-top:10px;"></input>
                 </form>

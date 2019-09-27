38d8fc2d73c8ce0ea14692e63ace43e1be457e9a
mar4elkin <googlcraftlol@gmail.com>
2019-09-27 16:52:40 +0300

    рабочая версия просмотра блога в админке

--- admin/view_text_page.php
+++ admin/view_text_page.php
@@ -3,39 +3,27 @@
       require_once("dbconnect.php");
       if(isset($_SESSION["rank"]) && ($_SESSION["rank"]) ==100   ) {
 
-      include '../template/admin.php';
-      $id_page = $_GET["id"];
-?>
-<style>@import url(../css/suitup.css); </style>
-<script src="../js/suitup.jquery.min.js"></script>
-<script src="../js/extended-commands.suitup.jquery.js"></script>
-      <script>
-      $( function(){
-      	$('.suitup-textarea')
-      		.suitUp()
-      });
-      </script>
+      include '../template/admin.php'; ?>
+          <div class="container">
+            <div class="row">
+          <?php
 
-      <section class="item-category-area section-gap">
-        <div class="container">
-      <?php
+            $result = $mysqli->query("SELECT * FROM `page_text`");
+            while ($row = $result->fetch_assoc()) {
+                  echo '
+                      <div class="col" style="margin:5px;">
+                        <ul style="width:450px;" class="list-group">
+                          <li class="list-group-item"><a href="http://'.$adsite.'/admin/view_text_page_in.php?id='. $row["id"] .'">'.$row["text_header"].'</a></li>
+                        </ul>
+                      </div>
+                        ';
+            }
 
-              $result = $mysqli->query("SELECT * FROM `page_text` WHERE `id` = $id_page");
-                while ($row = $result->fetch_assoc() ) {
-                  echo '<h4 class="border-bottom border-gray pb-2 mb-0" style="color:black;">  ' . $row["text_header"] .  '</h4>';
-                  echo '<div class="container" style="margin-top:10px;">';
-                  echo '<div class="row">';
-                  echo '<div class="col">';
-                  echo ' <p style="color:black;"> ' . html_entity_decode($row["text"]) . ' </p> ';
-                 }
-           ?>
-         </div>
-         </div>
-      </div>
-      </div>
-      </section>
-</div>
-</body>
+          ?>
+          </div>
+        </div>
+    </div>
+  </body>
 </html>
 <?php
 } else {
